9cc0e0052c87b2172a88288b80622126983297fa
Meaghan De Klerk <meaghan.dk@gmail.com>
2019-06-18 20:28:19 +1200

    first use of hook

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,33 +1,59 @@
-import React , { Fragment } from "react";
+import React , { Fragment, useState } from "react";
 import Greeting from "./Greeting"
 
 
-class App extends React.Component {
-    state = {
-            people: [ 
-            {name: "Bob"},
-            {name: "Jim"},
-            {name: "Jim-bob"} 
-        ]
-    }
+// class App extends React.Component {
+//     state = {
+//             people: [ 
+//             {name: "Bob"},
+//             {name: "Jim"},
+//             {name: "Jim-bob"} 
+//         ]
+//     }
 
-    changeNameHandler = () => {
-        console.log("Clicked")
-    }
-    render(){
+//     changeNameHandler = () => {
+//         this.setState({
+//             people: [ 
+//                 {name: "John"},
+//                 {name: "Johnny"},
+//                 {name: "Johnny Boy"} 
+//             ]
+//         })
+//     }
+
+const App = props => {
+   const [ peopleState, setPeople ] = useState({
+        people: [ 
+                    {name: "Bob"},
+                    {name: "Jim"},
+                    {name: "Jim-bob"} 
+                ],
+                other: "some stuff",
+            });
+
+    const [other, setOther] = useState("some stuff");
+    
+    const changeNameHandler = () => {
+                setPeople({
+                    people: [ 
+                        {name: "John"},
+                        {name: "Johnny"},
+                        {name: "Johnny Boy"} 
+                    ]
+                })
+            }
         return(
              <Fragment>
 
            <h1>We working!</h1>
-           <button onClick={this.changeNameHandler} >Change Name</button>
-           <Greeting name={this.state.people[0].name} />
-           <Greeting name={this.state.people[1].name}>G'day</Greeting>
-           <Greeting name={this.state.people[2].name} />
+           <button onClick={changeNameHandler} >Change Name</button>
+           <Greeting name={peopleState.people[0].name} />
+           <Greeting name={peopleState.people[1].name}>G'day</Greeting>
+           <Greeting name={peopleState.people[2].name} />
 
 
         </Fragment>
         );
     }
-}
 
 export default App;
