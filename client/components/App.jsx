6bd7a705f33122a905d50253002f431791952c2d
Meaghan De Klerk <meaghan.dk@gmail.com>
2019-06-18 17:22:23 +1200

    initial commit

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+import React , { Fragment } from "react";
+
+function App (props) {
+    return(
+        <Fragment>
+
+           <h1>We working!</h1>
+
+        </Fragment>
+       
+    )
+}
+
+export default App;
